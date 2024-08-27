bfecdc178a345cd5b87cfb3d710e80f8898be452
yunhee1 <alkrg007@naver.com>
2024-08-28 01:44:45 +0900

    feat: 메인화면 프레임, 디자인 작업

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../css/container.scss";
 import bg from "../img/bg-img.png";
 import logo from "../img/home-icon.png";
+import uploadIcon from "../img/gallery-icon.png";
+import modernImg from "../img/modern-img.png";
+import warmImg from "../img/warm-img.png";
+import minimalImg from "../img/minimal-img.png";
+import cozyImg from "../img/cozy-img.png";
+import colorfulImg from "../img/colorful-img.png";
+import animationImg from "../img/animation-img.png";
+import planeriorImg from "../img/planterior-img.png";
+import brightImg from "../img/bright-img.png";
 
 const Main = () => {
+  //   const [prompt, setPrompt] = useState("");
   return (
     <>
       <div className="container">
@@ -15,9 +26,121 @@ const Main = () => {
           <span style={{ color: "#9775FA" }}>빵꾸집꾸</span>에게 맡겨보세요!
         </div>
         <div className="upload-container">
-          <button>
-            <div>dk</div>
+          <button className="upload-button">
+            <div className="upload-icon-image-container">
+              <img
+                className="upload-icon-image"
+                src={uploadIcon}
+                alt="uploadIcon"
+              />
+            </div>
+            <div className="inner-button">
+              <div className="inner-button-text">방 사진 올리기</div>
+            </div>
           </button>
+          <div className="title-container">
+            <div className="title-text">
+              <span style={{ color: "#9775FA" }}>*</span>테마 선택
+            </div>
+          </div>
+
+          <div className="theme-buttons-container">
+            <div className="theme-four-buttons-container">
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={modernImg}
+                  alt="modernImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">모던한</div>
+                </div>
+              </div>
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={warmImg}
+                  alt="warmImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">따뜻한</div>
+                </div>
+              </div>
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={minimalImg}
+                  alt="minimalImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">미니멀한</div>
+                </div>
+              </div>
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={cozyImg}
+                  alt="cozyImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">아늑한</div>
+                </div>
+              </div>
+            </div>
+            <div className="theme-four-buttons-container">
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={colorfulImg}
+                  alt="colorfulImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">컬러풀한</div>
+                </div>
+              </div>
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={animationImg}
+                  alt="animationImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">동화같은</div>
+                </div>
+              </div>
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={planeriorImg}
+                  alt="planeriorImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">자연친화</div>
+                </div>
+              </div>
+              <div className="theme-button-container">
+                <img
+                  className="theme-button-img-container"
+                  src={brightImg}
+                  alt="brightImg"
+                />
+                <div className="theme-button-text-container">
+                  <div className="theme-button-text">화사한</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="title-container">
+            <div className="title-text">요청사항</div>
+          </div>
+          <textarea
+            type="text"
+            className="text-input"
+            placeholder="ex. 우울한 기분이 들지 않도록 밝은 분위기로 바꿔 줘."
+          />
+          <div className="generate-button">
+            <div className="inner-button-text">생성하기</div>
+          </div>
         </div>
       </div>
     </>
